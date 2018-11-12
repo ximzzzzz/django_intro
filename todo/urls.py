adeeee2765ec3b1020fbd68b8cc94b29e9d5ac13
ethan <ximzzzzz@gmail.com>
2018-11-12 06:45:19 +0000

    django_CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views #현재폴더(todo)에 있는 views 파일 추가
 
+
+app_name='todo'
+
 urlpatterns = [
-    path('', views.index), #/todos 가 이미 되있음
+    path('', views.index), #/todos 가 이미 되있음(생략)
     path('new/', views.new),
+    path('create/', views.create),
+    path('<int:id>/', views.read),
+    path('todo_create/', views.todo_create),
+    path('<int:id>/delete/', views.delete, name='delete'),
+    path('<int:id>/update/', views.update, name='update'),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todo.models import Todo
 
 
@@ -9,3 +9,44 @@ def index(request):
     
 def new(request):
     return render(request, 'todo/new.html')
+    
+def create(request):
+    title = request.POST.get('title')
+    deadline = request.POST.get('deadline')
+    todo = Todo(title=title, deadline=deadline)
+    todo.save() #add와 commit이 합쳐진 로직
+    
+    return redirect('/todos/')
+    
+def read(request,id):
+    todo = Todo.objects.get(id=id)
+    return render(request, 'todo/read.html',{'todo':todo})
+    
+def todo_create(request):
+    if request.method =='POST':
+        title = request.Post.get('title')
+        deadline = request.Post.get('deadline')
+        Todo.objects.create(title = title, deadline=deadline) #만들고 save까지 한번에!!
+        return redirect('/todos/')
+        
+    else:
+        return render(request, 'todo/todo_create.html')
+        
+def delete(request,id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/todos/')
+    
+def update(request,id):
+    todo = Todo.objects.get(id=id)
+    if request.method =='POST':
+        todo.title = request.Post.get('title')
+        todo.deadline = request.POST.get('deadline')
+        todo.save()
+        return redirect('/todos')
+        #저장로직
+        
+    else:
+        deadline = todo.deadline.strftime("%Y-%m-%d")
+        return render(request, 'todo/update.html',{'todo':todo, 'deadline':deadline})
+        #폼 보여주기 
